814400a461748f072c928c894a3438abfae1169d
BeomyoonHeo <gqd0512@naver.com>
2022-09-29 16:47:20 +0900

    product DAO 완료

--- src/main/java/site/metacoding/firstapp/domain/product/ProductDao.java
+++ src/main/java/site/metacoding/firstapp/domain/product/ProductDao.java
@@ -1,6 +1,20 @@
 package site.metacoding.firstapp.domain.product;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 public interface ProductDao {
+    public List<Product> findAll();
+
     public void insert();
+
+    public Product findById();
+
+    public void insert(Product product);
+
+    public void update(@Param("productId") Integer productiId, @Param("product") Product product);
+
+    public void deleteById(Integer productId);
 
 }
